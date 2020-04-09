d2b464a73b89a96470aa24eaeeab63c45d5fafef
Dmitriy Seriy <dima.seriy75@gmail.com>
2020-04-09 14:10:54 +0300

    fix finish comments!

--- Tesco/Tesco/pipelines.py
+++ Tesco/Tesco/pipelines.py
@@ -1,32 +1,73 @@
+import logging
+
 class TescoPipeline(object):
     def process_item(self, item, spider):
-        item['product_url'] = item['product_url'][0]
-        item['product_id'] = item['product_id'][0]
-        item['image_url'] = item['image_url'][0]
-        item['product_title'] = item['product_title'][0]
-        item['category'] = item['category'][0]
-        item['price'] = item['price'][0]
-
-        product_d = ''
-        list_d = item['product_d'][0]
-        for i in range(0, len(list_d)):
-            product_d += list_d[i] + ' '
-        item['product_d'] = product_d
-
-        item['pack_size'] = item['pack_size'][0]
-
-        manufacturer_address = ''
-        for i in item['manufacturer_address'][0]:
-            manufacturer_address += i + ' '
-        item['manufacturer_address'] = manufacturer_address
-
-        return_address = ''
-        for i in item['return_address'][0]:
-            return_address += i + ' '
-        item['return_address'] = return_address
-
-        item['net_contents'] = item['net_contents'][0]
-
+        try:
+            item['product_url'] = item['product_url'][0]
+        except Exception as e:
+            logging.info('Error in [product_url] : %s', e)
+            item['product_url'] = ''
+        try:
+            item['product_id'] = item['product_id'][0]
+        except Exception as e:
+            logging.info('Error in [product_id] : %s', e)
+            item['product_id'] = 0
+        try:
+            item['image_url'] = item['image_url'][0]
+        except Exception as e:
+            logging.info('Error in [image_url] : %s', e)
+            item['image_url'] = ''
+        try:
+            item['product_title'] = item['product_title'][0]
+        except Exception as e:
+            logging.info('Error in [product_title] : %s', e)
+            item['product_title'] = ''
+        try:
+            item['category'] = item['category'][0]
+        except Exception as e:
+            logging.info('Error in [category] : %s', e)
+            item['category'] = ''
+        try:
+            item['price'] = item['price'][0]
+        except Exception as e:
+            logging.info('Error in [price] : %s', e)
+            item['price'] = 0.0
+        try:
+            product_d = ''
+            list_d = item['product_d'][0]
+            for i in range(0, len(list_d)):
+                product_d += list_d[i] + ' '
+            item['product_d'] = product_d
+        except Exception as e:
+            logging.info('Error in [product_d] : %s', e)
+            item['product_d'] = ''
+        try:
+            item['pack_size'] = item['pack_size'][0]
+        except Exception as e:
+            logging.info('Error in [pack_size] : %s', e)
+            item['pack_size'] = ''
+        try:
+            manufacturer_address = ''
+            for i in item['manufacturer_address'][0]:
+                manufacturer_address += i + ' '
+            item['manufacturer_address'] = manufacturer_address
+        except Exception as e:
+            logging.info('Error in [manufacturer_address] : %s', e)
+            item['manufacturer_address'] = ''
+        try:
+            return_address = ''
+            for i in item['return_address'][0]:
+                return_address += i + ' '
+            item['return_address'] = return_address
+        except Exception as e:
+            logging.info('Error in [return_address] : %s', e)
+            item['return_address'] = ''
+        try:
+            item['net_contents'] = item['net_contents'][0]
+        except Exception as e:
+            logging.info('Error in [net_contents] : %s', e)
+            item['net_contents'] = ''
+        
         for i in item['review']:
             i['review_title'] = i['review_title'][0]
             i['stars_count'] = i['stars_count'][0]
--- Tesco/Tesco/spiders/TescoSpider.py
+++ Tesco/Tesco/spiders/TescoSpider.py
@@ -6,7 +6,7 @@ import scrapy
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from Tesco.items import TescoItem, TescoItemReview ,TescoItemNext
+from Tesco.items import TescoItem, TescoItemReview, TescoItemNext
 import openpyxl
 
 class TescoSpider(scrapy.Spider):
@@ -15,15 +15,15 @@ class TescoSpider(scrapy.Spider):
 
     def start_requests(self):
         try:
-            eBuffer = openpyxl.load_workbook(
-                'Quest_Links_for_the_Tesco_com_parser.xlsx')
-            list1 = eBuffer.get_sheet_by_name('Лист1')
-            urls_list = []
-            for i in range(1, 101):
-                urls_list.append(list1.cell(row=i, column=1).value)
+           eBuffer = openpyxl.load_workbook(
+               'Quest_Links_for_the_Tesco_com_parser.xlsx')
+           list1 = eBuffer.get_sheet_by_name('Лист1')
+           urls_list = []
+           for i in range(1, 101):
+               urls_list.append(list1.cell(row=i, column=1).value)
         except Exception as e:
-            self.logger.info('Error load XLSX file : %s', e)
-        #urls_list = ['https://www.tesco.com/groceries/en-GB/products/285811261']
+           self.logger.info('Error load XLSX file : %s', e)
+        #urls_list = ['https://www.tesco.com/groceries/en-GB/products/302664282']
         for url in urls_list:
             yield scrapy.Request(url=url, callback=self.parse_review)
 
@@ -45,19 +45,18 @@ class TescoSpider(scrapy.Spider):
             item['price'] = i.xpath(
                 './/div[contains(@class,"control-wrapper")]//span[@data-auto="price-value"][1]/text()').get(default="0.0"),
             item['product_d'] = i.xpath(
-                './/div[@class="product-blocks"]/div[@id="product-description"]/ul/li/text()').getall(),
+                './/div[@id="product-description"]/ul/li/text()').getall(),
             item['pack_size'] = i.xpath(
                 './/div[@class="product-blocks"]//li[contains(text(),"Pack")]/text()').get(default="")[11:],
             item['manufacturer_address'] = i.xpath(
-                './/div[@class="product-blocks"]//div[@id="manufacturer-address"]/ul/li/text()').getall(),
+                './/div[@id="manufacturer-address"]/ul/li/text()').getall(),
             item['return_address'] = i.xpath(
-                './/div[@class="product-blocks"]//div[@id="return-address"]/ul/li/text()').getall(),
+                './/div[@id="return-address"]/ul/li/text()').getall(),
             item['net_contents'] = i.xpath(
-                './/div[@class="product-blocks"]//div[@id="net-contents"]/p/text()').get(default=""),
+                './/div[@id="net-contents"]/p/text()').get(default=""),
             # формирование списка объектов Usually Bought Next Products (array of objects)
             try:
-                count_nodes_next = i.xpath(
-                    'count(//div[@class="recommender__wrapper"]/div[@class="product-tile-wrapper"])').getall()[0],
+                count_nodes_next = i.xpath('count(//div[@class="recommender__wrapper"]/div[@class="product-tile-wrapper"])').getall()[0],
                 count_nodes_next = int(float(count_nodes_next[0]))
             except Exception as e:
                 self.logger.info('Error : %s', e)
@@ -84,38 +83,13 @@ class TescoSpider(scrapy.Spider):
 
             # формирование списка объектов Review (array of objects) из JSON
             item['review'] = []
-            for val in prod_review_entries:
-                itemr = TescoItemReview()
-                # формирование review_title - заголовка отзыва
-                if val['summary'] == None:
-                    itemr['review_title'] = val['text'][:50] if val['text'] != None else '',
-                else:
-                    itemr['review_title'] = val['summary'],
-
-                # формирование stars_count - рейтинг звезд
-                itemr['stars_count'] = val['rating']['value'],
-
-                # формирование author - автор отзыва
-                if val['syndicationSource']['name'] == None:
-                    itemr['author'] = 'A Tesco Customer',
-                else:
-                    itemr['author'] = val['syndicationSource']['name'],
-
-                # формирование data - дата отзыва
-                tm1 = time.ctime(val['submissionTime']//1000)
-                dt = time.strptime(tm1, '%a %b %d %H:%M:%S %Y')
-                itemr['date'] = time.strftime('%dth %B %Y', dt),
-
-                # формирование review_text - тела(текст) отзыва
-                if val['text'] == None:
-                    itemr['review_text'] = '',
-                else:
-                    itemr['review_text'] = val['text'],
-                item['review'].append(itemr)
+            item['review'].extend(self.form_review(prod_review_entries))
+            
             if prod_review_info['count'] == 0 or (prod_review_info['total'] / prod_review_info['count'] == 1):
                 yield item
             else:
-                max_page = math.ceil(prod_review_info['total'] / prod_review_info['count']) + 1
+                max_page = math.ceil(
+                    prod_review_info['total'] / prod_review_info['count']) + 1
                 for next_page in range(2, max_page):
                     yield scrapy.Request("https://www.tesco.com/groceries/en-GB/reviews/{}?page={}".format(prod_id, next_page), callback=self.parse_item_review, headers={"Content-Type": "application/json", "Accept": "application/json, text/javascript, */*; q=0.01"}, meta={'collected_item': item, 'next_page': next_page, 'max_page': max_page, 'prod_id': prod_id})
 
@@ -126,14 +100,22 @@ class TescoSpider(scrapy.Spider):
         prod_id = response.meta['prod_id']
         data_props_json = json.loads(response.body)
         data_props_json = data_props_json["entries"]
-        for val in data_props_json:
+        item['review'].extend(self.form_review(data_props_json))       
+        if next_page != max_page:
+            next_page += 1
+            return scrapy.Request("https://www.tesco.com/groceries/en-GB/reviews/{}?page={}".format(prod_id, next_page), callback=self.parse_item_review, headers={"Content-Type": "application/json", "Accept": "application/json, text/javascript, */*; q=0.01"}, meta={'collected_item': item, 'next_page': next_page, 'max_page': max_page, 'prod_id': prod_id})
+        else:
+            return item
+
+    def form_review(self, list_review):
+        itemr_list = []
+        for val in list_review:
             itemr = TescoItemReview()
             # формирование review_title - заголовка отзыва
             if val['summary'] == None:
                 itemr['review_title'] = val['text'][:50] if val['text'] != None else '',
             else:
                 itemr['review_title'] = val['summary'],
-
             # формирование stars_count - рейтинг звезд
             itemr['stars_count'] = val['rating']['value'],
             # формирование author - автор отзыва
@@ -141,20 +123,14 @@ class TescoSpider(scrapy.Spider):
                 itemr['author'] = 'A Tesco Customer',
             else:
                 itemr['author'] = val['syndicationSource']['name'],
-
             # формирование data - дата отзыва
             tm1 = time.ctime(val['submissionTime']//1000)
             dt = time.strptime(tm1, '%a %b %d %H:%M:%S %Y')
             itemr['date'] = time.strftime('%dth %B %Y', dt),
-
             # формирование review_text - тела(текст) отзыва
             if val['text'] == None:
                 itemr['review_text'] = '',
             else:
                 itemr['review_text'] = val['text'],
-            item['review'].append(itemr)
-        if next_page != max_page:
-            next_page += 1
-            return scrapy.Request("https://www.tesco.com/groceries/en-GB/reviews/{}?page={}".format(prod_id, next_page), callback=self.parse_item_review, headers={"Content-Type": "application/json", "Accept": "application/json, text/javascript, */*; q=0.01"}, meta={'collected_item': item, 'next_page': next_page, 'max_page': max_page, 'prod_id': prod_id})
-        else:
-            return item
+            itemr_list.append(itemr)
+        return itemr_list
